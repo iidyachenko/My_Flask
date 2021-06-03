a773d3200278d26b2deb0358415c74c8e2b33b49
igor <igor.dyachenko@x5.ru>
2021-06-03 20:16:31 +0300

    Добавлена база данных

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,23 +1,23 @@
 from flask import Blueprint, render_template
 from werkzeug.exceptions import NotFound
+from flask_login import login_required
+
+from blog.models import User
 
 users = Blueprint("users", __name__, url_prefix='/users', static_folder='../static')
-USERS = {
-    1: "Игорь",
-    2: "Павел",
-    3: "Алексей",
-}
 
 
 @users.route("/")
 def users_list():
-    return render_template("users/list.html", users=USERS)
+    user_db = User.query.all()
+    return render_template("users/list.html", users=user_db)
 
 
 @users.route("/<int:user_id>/")
+@login_required
 def user_details(user_id: int):
     try:
-        user_name = USERS[user_id]
+        user = User.query.filter_by(id=user_id).one_or_none()
     except KeyError:
         raise NotFound(f"User #{user_id} doesn't exist!")
-    return render_template('users/detail.html', user_id=user_id, user_name=user_name)
+    return render_template('users/detail.html', user_id=user_id, user_name=user.email)
